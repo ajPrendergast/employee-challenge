84162e71fccb3db398a04ce1a73e2b8d1c1284b3
ajprendergast <ajprendergast99@gmail.com>
2025-10-06 23:31:44 -0400

    enable annotations

--- api/src/main/java/com/reliaquest/api/ApiApplication.java
+++ api/src/main/java/com/reliaquest/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.reliaquest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableCaching
+@EnableRetry
 public class ApiApplication {
 
     public static void main(String[] args) {
